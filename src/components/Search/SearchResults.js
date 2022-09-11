2b9dd2a82e711c224fbf73b20a4dfe0cad6bbcc2
Zeustakeshi <gacon2312@gmail.com>
2022-09-11 10:17:24 +0700

    add SearchResults component

---
+++ src/components/Search/SearchResults.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SearchResults = (props) => {
+    return <div></div>;
+};
+
+SearchResults.propTypes = {};
+
+export default SearchResults;
